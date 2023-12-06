2362e3bd3ad1ad4191db08e4bedfcd772c397bfa
minin_dm <minin_dm@mail.ru>
2023-12-06 22:31:51 +0300

    Создал работающий скрипт смены настроений (тестил на shell). Теперь требуется разобраться как настроить его работу на постоянной основе через планировщик

---
+++ backend/call_center/task.py
@@ -0,0 +1,14 @@
+# from celery import shared_task
+
+import random
+
+from call_center.models import Agent
+
+# @shared_task
+def agents_mood():
+    agents = Agent.objects.all()
+    for agent in agents:
+        agent.mood = random.choice(agent.MOOD_CHOICES)[0]
+        agent.save()
+
+agents_mood()
